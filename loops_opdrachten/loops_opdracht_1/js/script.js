b9f94c6127b07046b4e4aa2a02ab1da8a4b7609d
KiloulB <kiloulb@gmail.com>
2019-12-03 16:12:16 +0100

    Loop 1

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,12 @@
 //javascript code
+
+function showLoop() {
+    var a;
+    var text = "";
+  
+    for (a = 1; a <= 5; a++) {
+      console.log(a);
+      text += "Nummer " + a + "<br>";
+    }
+    document.getElementById("loop").innerHTML = text;
+  }
